76a6b77d982406a5a89f615d89d67fc9395fee1b
Harsh <harshraj0381@gmail.com>
2024-02-22 02:13:53 +0530

    dom

--- script.js
+++ script.js
@@ -3,89 +3,101 @@
 
 // ----------------------------------------> Datatypes <----------------------------
 
-const accountId = 125416;
-let accountEmail = "harshraj@gmail.com";
-var accountPassword = "1234943"; // Prefer not to use var because of issue in block scope and functional scope
-accountCity = "Patna";
-let accountState; // it will give undefined value
+// const accountId = 125416;
+// let accountEmail = "harshraj@gmail.com";
+// var accountPassword = "1234943"; // Prefer not to use var because of issue in block scope and functional scope
+// accountCity = "Patna";
+// let accountState; // it will give undefined value
 
 // // accountId = 4 // not allowed ---> const variable already defined
 
-accountEmail = "aman@gmail.com";
-accountPassword = "3665564";
-accountCity = "Mumbai";
+// accountEmail = "aman@gmail.com";
+// accountPassword = "3665564";
+// accountCity = "Mumbai";
 
-console.log(accountId);
+// console.log(accountId);
 
-console.table([
-  accountId,
-  accountEmail,
-  accountPassword,
-  accountCity,
-  accountState,
-]);
+// console.table([
+//   accountId,
+//   accountEmail,
+//   accountPassword,
+//   accountCity,
+//   accountState,
+// ]);
 
 // ------------------------------------> Conversion operation <--------------------
 // Link to study typeConversion ---> https://tc39.es/ecma262/multipage/abstract-operations.html
 
-let score = "33abc";
-console.log(typeof score);
+// let score = "33abc";
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => Nan (Not a Number)
 // true => 1; false => 0
 
-let isLoggedIn = "aman";
+// let isLoggedIn = "aman";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // // 1 => true; 0 => false
 // // "" => false
 // // "aman" => true
 
-let sumNumber = 33;
+// let sumNumber = 33;
 
-let stringNumber = String(sumNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(sumNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-//          -----------------------------> operations <-----------------------------
+//          --------------------------------> Operations <-----------------------------
 
-let value = 3;
-let negValue = -value; // negative Value
-console.log(negValue);
+// let value = 3;
+// let negValue = -value; // negative Value
+// console.log(negValue);
 
-console.log(4 + 5);
-console.log(9 - 5);
-console.log(4 * 5);
-console.log(4 / 5);
-console.log(18 % 5);
+// console.log(4 + 5);
+// console.log(9 - 5);
+// console.log(4 * 5);
+// console.log(4 / 5);
+// console.log(18 % 5);
 
-let str1 = "Hello";
-let str2 = " Harsh";
-let str3 = str1 + str2;
-console.log(str3);
+// let str1 = "Hello";
+// let str2 = " Harsh";
+// let str3 = str1 + str2;
+// console.log(str3);
 
-console.log("5" + 4); // --> 54
-console.log(5 + "4"); // --> 54
-console.log("5" + 4 + 4); // --> 544
-console.log(5 + 4 + "4"); // --> 94
+// console.log("5" + 4); // --> 54
+// console.log(5 + "4"); // --> 54
+// console.log("5" + 4 + 4); // --> 544
+// console.log(5 + 4 + "4"); // --> 94
 
-console.log(true); // --> true
-console.log(+true); // --> 1
-// console.log(true+); // --> error
-console.log(+""); // --> 0
+// // console.log(true); // --> true
+// // console.log(+true); // --> 1
+// // console.log(true+); // --> error
+// console.log(+""); // --> 0
 
-let num1, num2, num3;
-num1 = num2 = num3 = 2 + 2;
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
 
-let x = 100;
-x++; // postfix
-++x; // prefix
-console.log(x);
+// let x = 100;
+// x++; // postfix
+// ++x; // prefix
+// console.log(x);
 // Link to study Increament ---> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// --------------------------------------> Comparison of datatypes <-----------------------------------
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log();
